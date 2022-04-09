d10810d2b63aaab0d2c369b71e7f33681ec860f5
aasim bhat <90784719+Aasimbhat@users.noreply.github.com>
2022-04-09 10:23:41 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart'; //material.dart used to build widget tree
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
