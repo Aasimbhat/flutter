6295d1cb5fa56fc704c544044b4ce53c8ab19ffd
aasim bhat <90784719+Aasimbhat@users.noreply.github.com>
2022-04-13 16:04:17 -0700

    day6 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_toturail/pages/login_page.dart';
+import 'package:flutter_toturail/utlis/routes.dart';
+
 
 import 'homepage.dart'; //material.dart used to build widget tree
 
@@ -10,22 +12,26 @@ void main(){ //main is a function
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 //$ means string interpolation
-   
+                                                                      
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       // home: homePage(),
       themeMode:ThemeMode.dark ,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      
+      ),
+     debugShowCheckedModeBanner: false,
+      
       darkTheme: ThemeData(
         brightness: Brightness.light,
       ),
       // initialRoute: "/home", //if we want home page should be first so we use initail route
       routes:{
         "/":(context)=>LogInPage(), // / by default means home route
-          "/home":(context)=>homePage(),
-        "/login" :(context)=>LogInPage()
+          MyRoutes.homeRoute:(context)=>homePage(),
+        MyRoutes.loginRoute :(context)=>LogInPage()
       } , //first it takes map(which has key and value)first key it takes as string and second it takes function(it has parameter build context)
     );
   } 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,105 @@
 import 'package:flutter/material.dart';
-class LogInPage extends StatelessWidget { //class is a blueprint --class generates objec
-  
+import 'package:flutter_toturail/utlis/routes.dart';
 
+class LogInPage extends StatefulWidget {
+  
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  
+  bool changeButton=false;
+  
+  //class is a blueprint --class generates object
+  
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),),
-        
-        ),
-    );
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.cover,
+                                                              
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: " Enter Username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: " Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                      SizedBox(
+                height: 40.0,
+              ),
+
+              InkWell(
+                onTap: ()async {
+                  setState(() {
+                    
+                    changeButton=true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
+                },
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 1),
+                  width:changeButton ? 50: 150,
+                  height: 50,
+                  // color: Colors.deepPurple,
+                  alignment: Alignment.center,
+                  child: changeButton?Icon(Icons.done,color: Colors.white,) : Text("Login",
+                  style: TextStyle(color:Colors.white,fontWeight:FontWeight.bold,fontSize: 18),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    // shape: changeButton
+                    // ?BoxShape.circle
+                    // :BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(changeButton ?20: 10)
+                  ),
+                ),
+              )
+              // ElevatedButton(
+              //   onPressed: (){
+              //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+              //   },
+              //   style: TextButton.styleFrom(minimumSize: Size(150,40 )),
+              //    child: Text("login")
+                 
+              //    )
+                  ],
+                  
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
